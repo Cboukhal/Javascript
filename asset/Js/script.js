a785e8459759a66f69f9f265e4a9bd6b924ea08a
Cboukhal <boukhalfa.camil@hotmail.fr>
2025-05-13 13:05:28 +0200

    JS

--- asset/Js/script.js
+++ asset/Js/script.js
@@ -3,3 +3,17 @@ document.write("<h2>Je suis du JS externe.</h2>");
 //commentaire monoligne
 /*comentaire 1
 commentaire 2*/
+document.write("<h2>Les variables</h2>");
+let prenom; //déclaration de la variable prenom avec let
+var nom; //déclaration de la variable prenom avec var
+prenom = "Ludovic"; //affectation ou assignation
+nom = "Dupont";//affectation ou assignation
+document.write("Bonjour ",prenom," ",nom," bienvenue sur JS.");
+let a, b, c, d;
+a = 130;
+b = "coucou";
+c = true;
+document.write("<br>La valeur de a : ",a," "," son type :", typeof(a));
+document.write("<br>La valeur de b : ",b," "," son type :", typeof(b));
+document.write("<br>La valeur de c : ",c," "," son type :", typeof(c));
+document.write("<br>La valeur de d : ",d," "," son type :", typeof(d));
